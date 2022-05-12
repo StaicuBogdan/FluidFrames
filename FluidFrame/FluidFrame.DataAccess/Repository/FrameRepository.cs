3fd0b91efd6213d0bb04ba36cf7f39a0246a441c
StaicuBogdan <61222625+StaicuBogdan@users.noreply.github.com>
2022-05-12 22:12:29 +0300

    Frame Model, Repository added

---
+++ FluidFrame/FluidFrame.DataAccess/Repository/FrameRepository.cs
@@ -0,0 +1,46 @@
+﻿using FluidFrame.DataAccess.Data;
+using FluidFrame.DataAccess.Repository.IRepository;
+using FluidFrame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluidFrame.DataAccess.Repository
+{
+    public class FrameRepository : Repository<Frame>, IFrameRepository
+    {
+        private ApplicationDbContext _db;
+
+        public FrameRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Frame obj)
+        {
+            var frameDb = _db.Frames.FirstOrDefault(u => u.Id == obj.Id);
+            if (frameDb != null)
+            {
+                frameDb.Description = obj.Description;
+                frameDb.ModelName = obj.ModelName;
+                frameDb.OwnerPhoneNumber = obj.OwnerPhoneNumber;
+
+                frameDb.Price = obj.Price;
+                frameDb.Price7Days = obj.Price7Days;
+
+                frameDb.Latitude = obj.Latitude;
+                frameDb.Longitude = obj.Longitude;
+
+                frameDb.CategoryId = obj.CategoryId;
+                frameDb.FrameTypeId = obj.FrameTypeId;
+
+                if (obj.ImageUrl != null)
+                {
+                    frameDb.ImageUrl = obj.ImageUrl;
+                }
+            }
+        }
+    }
+}
---
+++ FluidFrame/FluidFrame.Models/Frame.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FluidFrame.Models
+{
+    public class Frame
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string ModelName { get; set; }
+        [Required]
+        public string OwnerPhoneNumber { get; set; }
+        public string ImageUrl { get; set; }
+        [Required]
+        public double Latitude { get; set; }
+        [Required]
+        public double Longitude { get; set; }
+
+
+        [Required]
+        [Range(1, 99999)]
+        public double Price { get; set; }
+        [Required]
+        [Range(1, 99999)]
+        public double Price7Days { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        [Required]
+        public int FrameTypeId { get; set; }
+        public FrameType FrameType { get; set; }
+    }
+}
